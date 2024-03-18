dfa6c702b1774e7e35e27aa297eeed46a10b367b
Osbiju <oscarbierge@gmail.com>
2024-03-18 11:14:35 +0100

    añado descipcion al endpoint

--- src/main/java/cat/itacademy/barcelonactiva/BiergeOscar/s05t01n02/controllers/FlorController.java
+++ src/main/java/cat/itacademy/barcelonactiva/BiergeOscar/s05t01n02/controllers/FlorController.java
@@ -3,6 +3,7 @@ package cat.itacademy.barcelonactiva.BiergeOscar.s05t01n02.controllers;
 import cat.itacademy.barcelonactiva.BiergeOscar.s05t01n02.model.domain.FlorEntity;
 import cat.itacademy.barcelonactiva.BiergeOscar.s05t01n02.model.dto.FlorDTO;
 import cat.itacademy.barcelonactiva.BiergeOscar.s05t01n02.model.services.FlorService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ public class FlorController {
 
     //add
     @PostMapping("/add")
+    @Operation(summary = "Crea una nova sucursal", description = "Aquesta API et permet crear una flor")
     public ResponseEntity<FlorDTO> createFlor (@RequestBody FlorDTO florDTO){
         FlorDTO newFlor = florService.createFlor(florDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(newFlor);
